26b367c1b1a830a8a10063000f9f04bfd2a7b5bd
Claudio Sarto <claudiosarto@gmail.com>
2024-01-05 17:31:30 +0000

    feat: add deckbuild function

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+function buildDeck(){
+    let deck = [];
+    let cardValues = ["a", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"];
+    let cardTypes = ["c", "d", "h", "s"];
+
+    for (let i = 0; i < cardTypes.length; i++) {
+        for (let j = 0; j < cardValues.length; j++) {
+            deck.push(cardValues[j] + "-" + cardTypes[i]);
+        }
+    }
+    return deck;
+}
+
+window.onload = function() {
+    mainFunction();
+}
+
+function mainFunction(){   
+    deck = buildDeck();
+    console.log(deck);
+}
+
