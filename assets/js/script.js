eb3afc6bdc6dc5f9424a37dc70e07450d601e5e0
Claudio Sarto <claudiosarto@gmail.com>
2024-01-05 18:49:10 +0000

    feat: add getcardvalue feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+window.onload = function() {
+    mainFunction();
+}
+
+function mainFunction() {   
+    let dealerSum = 0;
+    let playerSum = 0;
+    let dealerAces = 0;
+    let playerAces = 0;
+    let deck = buildDeck();
+    //console.log(deck);    
+    let dealerHidden = deck.splice([Math.floor(Math.random()*deck.length)],1);
+
+
+    //console.log(dealerHidden);
+    //console.log(deck);
+    ;
+}
+
 function buildDeck(){
     let deck = [];
     let cardValues = ["a", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"];
@@ -11,17 +30,21 @@ function buildDeck(){
     return deck;
 }
 
-window.onload = function() {
-    mainFunction();
+function getCardValue(card){
+    let data = card.split("-"); // get digits before "-"  
+    
+    // return related value based on digit  
+    switch(data[0]){
+        case "a":
+            return 11;
+        
+        case "j":
+        case "k":
+        case "q":   
+            return 10;
+        
+        default:
+            return parseInt(data[0]); 
+    }
+
 }
-
-function mainFunction() {   
-    let dealerSum = 0;
-    let playerSum = 0;
-    let deck = buildDeck();
-    //console.log(deck);
-}
-
-
-
-// pick random card from deck: deck.splice([Math.floor(Math.random()*deck.length)],1); 
